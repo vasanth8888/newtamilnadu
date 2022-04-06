5b29f3d568f2f6efc0f7d1fd32af1acb0a2f6d3a
vasanth8888 <vasanthveeda@09gmail.com>
2022-04-06 23:23:44 +0530

    tamilnadu

---
+++ src/test/java/org/p/South1.java
@@ -0,0 +1,20 @@
+package org.p;
+
+public class South1 {
+	
+	
+	private void thangamani1() {
+		// TODO Auto-generated method stub
+
+	}
+	private void latha2() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
